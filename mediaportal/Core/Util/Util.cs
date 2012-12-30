7d95c514425e32f1bdc52648284001e3cd7bd126
dedahr <dedahr@gmail.com>
2012-12-30 17:25:24 +0100

    Sort using shlwapi.dll (better sort result than custom sort code)

--- mediaportal/Core/Util/Util.cs
+++ mediaportal/Core/Util/Util.cs
@@ -113,6 +113,15 @@ namespace MediaPortal.Util
     [DllImport("mpr.dll")]
     private static extern int WNetAddConnection2A(ref NetResource pstNetRes, string psPassword, string psUsername,
                                                   int piFlags);
+    
+    /// <summary>
+    /// Sort strings obeying numerical values in strings
+    /// </summary>
+    /// <param name="str1"></param>
+    /// <param name="str2"></param>
+    /// <returns></returns>
+    [DllImport("shlwapi.dll", CharSet = CharSet.Unicode)]
+    public static extern int StrCmpLogicalW(string str1, string str2);
 
 
     private const int SW_SHOWNORMAL = 1;
@@ -4664,6 +4673,7 @@ namespace MediaPortal.Util
     }
   }
 
+  
   // (c) Vasian Cepa 2005
   // http://madebits.com
 
@@ -4674,174 +4684,209 @@ namespace MediaPortal.Util
     private bool zeroesFirst = false;
 
     private StringLogicalComparer()
-      {}
+    {
+    }
 
     private StringLogicalComparer(bool zeroesFirst)
-      {
-		this.zeroesFirst = zeroesFirst;
-	}
+    {
+      this.zeroesFirst = zeroesFirst;
+    }
 
     public static IComparer Default
-  {
+    {
       get { return _default; }
-  }
+    }
 
     public static IComparer DefaultZeroesFirst
-	{
-		get { return _defaultZeroesFirst; }
-  }
+    {
+      get { return _defaultZeroesFirst; }
+    }
 
     public int Compare(object x, object y)
-  {
+    {
       if (null == x && null == y) return 0;
       if (null == x) return -1;
       if (null == y) return 1;
+
       if (x is string && y is string)
-          return Compare((string)x, (string)y, zeroesFirst);
+        return Compare((string)x, (string)y, zeroesFirst);
+
       return Comparer.Default.Compare(x, y);
-  }
+    }
 
     public static int Compare(string s1, string s2)
-  {
-    return Compare(s1, s2, false);
-	}
+    {
+      return Compare(s1, s2, false);
+    }
 
     public static int Compare(string s1, string s2, bool zeroesFirst)
-  {
-		//get rid of special cases
-		if((s1 == null) && (s2 == null)) return 0;
-		else if(s1 == null) return -1;
-		else if(s2 == null) return 1;
+    {
+      if (null == s1 || 0 == s1.Length)
+      {
+        if (null == s2 || 0 == s2.Length) return 0;
+        return -1;
+      }
+      else if (null == s2 || 0 == s2.Length)
+      {
+        return 1;
+      }
 
-		if((s1.Length <= 0) && (s2.Length <= 0)) return 0;
-		else if(s1.Length <= 0) return -1;
-		else if(s2.Length <= 0) return 1;
+      int s1Length = s1.Length;
+      int s2Length = s2.Length;
 
-		//special case
-		bool sp1 = Char.IsLetterOrDigit(s1[0]);
-		bool sp2 = Char.IsLetterOrDigit(s2[0]);
-		if(sp1 && !sp2) return 1;
-		if(!sp1 && sp2) return -1;
+      bool sp1 = char.IsLetterOrDigit(s1[0]);
+      bool sp2 = char.IsLetterOrDigit(s2[0]);
 
-		int i1 = 0, i2 = 0; //current index
-		int r = 0; // temp result
-		char c1, c2;
-		bool letter1, letter2;
+      if (sp1 && !sp2) return 1;
+      if (!sp1 && sp2) return -1;
 
-		while(true)
-		{
-			c1 = s1[i1];
-			c2 = s2[i2];
-			sp1 = Char.IsDigit(c1);
-			sp2 = Char.IsDigit(c2);
-			if(!sp1 && !sp2)
-			{
-				letter1 = Char.IsLetter(c1);
-				letter2 = Char.IsLetter(c2);
+      char c1, c2;
+      int i1 = 0, i2 = 0;
+      int r = 0;
+      bool letter1, letter2;
 
-				if(letter1 && letter2)
-				{
-					r = Char.ToUpper(c1).ToString().CompareTo(Char.ToUpper(c2).ToString());
-					if(r != 0) return r;
-				}
-				else if(!letter1 && !letter2)
-				{
-					r = c1.CompareTo(c2);
-					if(r != 0) return r;
-				}
-				else if(!letter1 && letter2)
-				{
-					return -1;
-				}
-				else if(letter1 && !letter2)
-				{
-					return 1;
-				}
-			}
-			else if(sp1 && sp2)
-			{
-				r = CompareNum(s1, ref i1, s2, ref i2, zeroesFirst);
-				if(r != 0) return r;
-			}
-			else if(sp1)
-			{
-				return -1;
-			}
-			else if(sp2)
-			{
-				return 1;
-			}
-			i1++;
-			i2++;
-			if((i1 >= s1.Length) && (i2 >= s2.Length))
-			{
-				return 0;
-			}
-			else if(i1 >= s1.Length)
-			{
-				return -1;
-			}
-			else if(i2 >= s2.Length)
-			{
-				return 1;
-			}
-		}
-	}
+      while (true)
+      {
+        c1 = s1[i1];
+        c2 = s2[i2];
 
-	  private static int CompareNum(string s1, ref int i1, string s2, ref int i2, bool zeroesFirst)
-		{
-			int nzStart1 = i1, nzStart2 = i2; // nz = non zero
-			int end1 = i1, end2 = i2;
+        sp1 = char.IsDigit(c1);
+        sp2 = char.IsDigit(c2);
 
-			ScanNumEnd(s1, i1, ref end1, ref nzStart1);
-			ScanNumEnd(s2, i2, ref end2, ref nzStart2);
-			int start1 = i1; i1 = end1 - 1;
-			int start2 = i2; i2 = end2 - 1;
+        if (!sp1 && !sp2)
+        {
+          if (c1 != c2)
+          {
+            letter1 = char.IsLetter(c1);
+            letter2 = char.IsLetter(c2);
 
-			if(zeroesFirst)
-			{
-				int zl1 = nzStart1 - start1;
-				int zl2 = nzStart2 - start2;
-				if(zl1 > zl2) return -1;
-				if(zl1 < zl2) return 1;
-			}
+            if (letter1 && letter2)
+            {
+              c1 = char.ToUpper(c1);
+              c2 = char.ToUpper(c2);
 
-			int nzLength1 = end1 - nzStart1;
-			int nzLength2 = end2 - nzStart2;
+              r = c1 - c2;
+              if (0 != r) return r;
+            }
+            else if (!letter1 && !letter2)
+            {
+              r = c1 - c2;
+              if (0 != r) return r;
+            }
+            else if (letter1)
+            {
+              return 1;
+            }
+            else if (letter2)
+            {
+              return -1;
+            }
+          }
 
-			if(nzLength1 < nzLength2) return -1;
-			else if(nzLength1 > nzLength2) return 1;
+        }
+        else if (sp1 && sp2)
+        {
+          r = CompareNumbers(s1, s1Length, ref i1, s2, s2Length, ref i2, zeroesFirst);
+          if (0 != r) return r;
+        }
+        else if (sp1)
+        {
+          return -1;
+        }
+        else if (sp2)
+        {
+          return 1;
+        }
 
-			for(int j1 = nzStart1,j2 = nzStart2; j1 <= i1; j1++,j2++)
-			{
-				int r = s1[j1].CompareTo(s2[j2]);
-				if(r != 0) return r;
-			}
-			// the nz parts are equal
-			int length1 = end1 - start1;
-			int length2 = end2 - start2;
-			if(length1 == length2) return 0;
-			if(length1 > length2) return -1;
-			return 1;
-		}
+        i1++;
+        i2++;
 
-		//lookahead
-	  private static void ScanNumEnd(string s, int start, ref int end, ref int nzStart)
-		{
-			nzStart = start;
-			end = start;
-			bool countZeros = true;
-			while(Char.IsDigit(s, end))
-			{
-				if(countZeros && s[end].Equals('0'))
-				{
-					nzStart++;
-				}
-				else countZeros = false;
-				end++;
-				if(end >= s.Length) break;
-			}
-		}
-	}
+        if (i1 >= s1Length)
+        {
+          if (i2 >= s2Length) return 0;
+          return -1;
+        }
+        else if (i2 >= s2Length)
+        {
+          return 1;
+        }
+      }
+    }
 
+    private static int CompareNumbers(
+        string s1, int s1Length, ref int i1,
+        string s2, int s2Length, ref int i2,
+        bool zeroesFirst)
+    {
+      int nzStart1 = i1, nzStart2 = i2;
+      int end1 = i1, end2 = i2;
+
+      ScanNumber(s1, s1Length, i1, ref nzStart1, ref end1);
+      ScanNumber(s2, s2Length, i2, ref nzStart2, ref end2);
+
+      int start1 = i1;
+      i1 = end1 - 1;
+      int start2 = i2;
+      i2 = end2 - 1;
+
+      if (zeroesFirst)
+      {
+        int zl1 = nzStart1 - start1;
+        int zl2 = nzStart2 - start2;
+        if (zl1 > zl2) return -1;
+        if (zl1 < zl2) return 1;
+      }
+
+      int length1 = end2 - nzStart2;
+      int length2 = end1 - nzStart1;
+
+      if (length1 == length2)
+      {
+        int r;
+        for (int j1 = nzStart1, j2 = nzStart2; j1 <= i1; j1++, j2++)
+        {
+          r = s1[j1] - s2[j2];
+          if (0 != r) return r;
+        }
+
+        length1 = end1 - start1;
+        length2 = end2 - start2;
+
+        if (length1 == length2) return 0;
+      }
+
+      if (length1 > length2) return -1;
+      return 1;
+    }
+
+    private static void ScanNumber(string s, int length, int start, ref int nzStart, ref int end)
+    {
+      nzStart = start;
+      end = start;
+
+      bool countZeros = true;
+      char c = s[end];
+
+      while (true)
+      {
+        if (countZeros)
+        {
+          if ('0' == c)
+          {
+            nzStart++;
+          }
+          else
+          {
+            countZeros = false;
+          }
+        }
+
+        end++;
+        if (end >= length) break;
+
+        c = s[end];
+        if (!char.IsDigit(c)) break;
+      }
+    }
+  }
 }
